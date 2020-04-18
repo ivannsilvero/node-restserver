2f54551e4bc6fca1e4b1a55cce92f12bb70fdee8
Iván <ivannsilvero@gmail.com>
2020-04-17 21:14:31 -0300

    Rest Server -usuario

--- server/config/config.js
+++ server/config/config.js
@@ -1,1 +1,13 @@
 process.env.PORT = process.env.PORT || 3000;
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+let urlDB;
+
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe';
+} else {
+    urlDB = 'mongodb+srv://cafe-user:abc123456@cluster0-dtzol.mongodb.net/test?retryWrites=true&w=majority';
+}
+
+process.env.URLDB = urlDB;
